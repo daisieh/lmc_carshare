27dd29b8561668044f08cdf7aba4100e148ac034
Jordan Place <tapman90@gmail.com>
2019-07-24 16:03:44 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# check_my_cal
+
+check_my_cal is a "non-hosted app" for testing the [Transposit JavaScript SDK](https://github.com/transposit/transposit-js-sdk).
---
+++ src/index.tsx
@@ -0,0 +1,92 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Transposit } from "transposit";
+
+import "./styles.css";
+
+const transposit = new Transposit("https://addcred-mo4no.demo-transposit.com");
+
+function SignIn() {
+  return (
+    <a
+      href={transposit.startLoginUri(
+        `${window.location.origin}/signin/handle-redirect`
+      )}
+    >
+      Sign in
+    </a>
+  );
+}
+
+function SignInHandleRedirect() {
+  React.useEffect(() => {
+    try {
+      transposit.handleLogin(() => {
+        // Specify where to redirect after sign-in completes
+        window.location.href = "/";
+      });
+    } catch (err) {
+      console.log(err);
+      window.location.href = "/signin";
+    }
+  }, []);
+
+  return null;
+}
+
+function Index() {
+  const [result, setResult] = React.useState(null);
+  React.useEffect(() => {
+    transposit
+      .runOperation("hello_world")
+      .then(response => {
+        if (response.status !== "SUCCESS") {
+          throw response;
+        }
+        setResult(JSON.stringify(response.result.results));
+      })
+      .catch(response => {
+        console.log(response);
+      });
+  }, []);
+
+  if (!transposit.isLoggedIn()) {
+    window.location.href = "/signin";
+    return null;
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello {transposit.getUserEmail()}</h1>
+      {result && <h2>{result}</h2>}
+      <div>
+        <a href={transposit.settingsUri()}>settings</a>
+      </div>
+      <div>
+        <button
+          onClick={() => transposit.logOut(`${window.location.origin}/signin`)}
+        >
+          logout
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Route path="/signin" exact component={SignIn} />
+      <Route
+        path="/signin/handle-redirect"
+        exact
+        component={SignInHandleRedirect}
+      />
+      <Route path="/" exact component={Index} />
+    </Router>
+  );
+}
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
