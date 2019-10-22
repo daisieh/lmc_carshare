c3f23e42b11f5831a81a09f0db013cecc9f2b386
Jordan Place <jplace@transposit.com>
2019-10-22 08:48:17 -0700

    Run against 2.0.0-rc1 interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,75 +1,104 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Transposit } from "transposit";
+import { Transposit, User } from "transposit";
 import "./styles.css";
 
 const transposit = new Transposit(
   "https://check-my-cal-ja22m.staging-transposit.com"
 );
 
+function useSignedIn(): boolean {
+  const [isLoggedIn, setIsLoggedIn] = React.useState<boolean>(false);
+  React.useEffect(() => {
+    if (!transposit.isSignedIn()) {
+      window.location.href = "/signin";
+      return;
+    }
+    setIsLoggedIn(true);
+  }, []);
+  return isLoggedIn;
+}
+
+function useUser(isSignedIn: boolean): User | null {
+  const [user, setUser] = React.useState<User | null>(null);
+  React.useEffect(() => {
+    if (!isSignedIn) {
+      return;
+    }
+    transposit
+      .loadUser()
+      .then(u => setUser(u))
+      .catch(response => console.log(response));
+  }, [isSignedIn]);
+  return user;
+}
+
 function SignIn() {
   return (
-    <a
-      href={transposit.startLoginUri(
-        `${window.location.origin}/signin/handle-redirect`
-      )}
+    <button
+      onClick={async e => {
+        e.preventDefault();
+        await transposit.signIn(
+          `${window.location.origin}/signin/handle-redirect`
+        );
+      }}
     >
-      Sign in
-    </a>
+      Sign In
+    </button>
   );
 }
 
 function SignInHandleRedirect() {
   React.useEffect(() => {
-    try {
-      transposit.handleLogin(({ needsKeys }) => {
+    transposit.handleSignIn().then(
+      ({ needsKeys }) => {
         if (needsKeys) {
           window.location.href = transposit.settingsUri(window.location.origin);
         } else {
           window.location.href = "/";
         }
-      });
-    } catch (err) {
-      console.log(err);
-      window.location.href = "/signin";
-    }
+      },
+      () => {
+        window.location.href = "/signin";
+      }
+    );
   }, []);
-
   return null;
 }
 
 function Index() {
-  const [result, setResult] = React.useState<string | null>(null);
+  const isSignedIn = useSignedIn();
+  const user = useUser(isSignedIn);
+  const [opResult, setOpResult] = React.useState<string | null>(null);
   React.useEffect(() => {
+    if (!isSignedIn) {
+      return;
+    }
     transposit
-      .runOperation("load_todays_events")
-      .then(response => {
-        if (response.status !== "SUCCESS") {
-          throw response;
-        }
-        setResult(JSON.stringify(response.result.results));
+      .run("load_todays_events")
+      .then(({ results }) => {
+        setOpResult(JSON.stringify(results));
       })
       .catch(response => {
         console.log(response);
       });
-  }, []);
+  }, [isSignedIn]);
 
-  if (!transposit.isLoggedIn()) {
-    window.location.href = "/signin";
+  if (!isSignedIn || !user) {
     return null;
   }
 
   return (
     <div className="App">
-      <h1>Hello {transposit.getUserEmail()}</h1>
-      {result && <h2>{result}</h2>}
+      <h1>Hello {JSON.stringify(user)}</h1>
+      {opResult && <h2>{opResult}</h2>}
       <div>
         <a href={transposit.settingsUri()}>settings</a>
       </div>
       <div>
         <button
-          onClick={() => transposit.logOut(`${window.location.origin}/signin`)}
+          onClick={() => transposit.signOut(`${window.location.origin}/signin`)}
         >
           logout
         </button>
