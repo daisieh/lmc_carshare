0ead1087e972f30a2fc856ca7f789521989a0288
Daisie Huang <daisie@transposit.com>
2020-01-28 06:10:02 +0000

    Commit from Transposit IDE

--- backend/get_cars_available_for_time.js
+++ backend/get_cars_available_for_time.js
@@ -27,8 +27,8 @@
       }
     }
   }
-  return available_cars.map((x) => { return { Description: x.Description, Email: x.Email }; });
-  return api.run("this.get_features_list", { cars: available_cars });
+  //return available_cars.map((x) => { return { Description: x.Description, Email: x.Email }; });
+  //return api.run("this.get_features_list", { cars: available_cars });
   return available_cars;
 }
 
