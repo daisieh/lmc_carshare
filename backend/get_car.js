8594eaf5e5e6afac5ec27fb1fa755ba65cc82262
Daisie Huang <daisie@transposit.com>
2020-01-29 09:11:01 +0000

    Commit from Transposit IDE

--- backend/get_car.js
+++ backend/get_car.js
@@ -2,7 +2,7 @@
   let cars = api.run("this.list_cars")[0];
   let keys = Object.keys(cars);
   let index = keys.indexOf(params.vehicle);
-  if (index > 0) {
+  if (index >= 0) {
     return cars[params.vehicle];
   }
   return null;
