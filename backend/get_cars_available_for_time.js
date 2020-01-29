85c17375d1d00975bdb6745b9a17039c42a6c966
Daisie Huang <daisie@transposit.com>
2020-01-29 03:10:53 +0000

    Commit from Transposit IDE

--- backend/get_cars_available_for_time.js
+++ backend/get_cars_available_for_time.js
@@ -3,6 +3,7 @@
   let calendars = api.run("this.list_car_calendarlist")[0];
   let startTime = moment.tz(params.start, "America/Vancouver").format();
   let endTime = moment.tz(params.end, "America/Vancouver").format();
+  console.log(`looking for cars between ${startTime} and ${endTime}`);
   let cars = api.run("this.list_cars")[0];
   let available_cars = [];
   let calendar_ids = [];
@@ -16,13 +17,13 @@
     if (!cars[i].AlwaysAvailable) {
       console.log(`checking to see if (default busy) ${cars[i].Description} is busy...`);
       if (freebusy.calendars[calendars[i].id].busy.length > 0) {
-        console.log(`...listed as busy, so it's available`);
+        // console.log(`...listed as busy, so it's available`);
         available_cars.push(cars[i]);
       }
     } else {
       console.log(`checking to see if (default available) ${cars[i].Description} is busy...`);
       if (freebusy.calendars[calendars[i].id].busy.length == 0) {
-        console.log("...not busy, so it's available");
+        // console.log("...not busy, so it's available");
         available_cars.push(cars[i]);
       }
     }
