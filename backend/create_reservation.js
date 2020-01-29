d955a8c1567b5372983388cd8ccf36d267df1b0e
Daisie Huang <daisie@transposit.com>
2020-01-29 08:35:45 +0000

    Commit from Transposit IDE

---
+++ backend/create_reservation.js
@@ -0,0 +1,22 @@
+(params) => {
+  let calendar_id = api.run("this.get_calendarlist", {vehicle: params.vehicle})[0].id;
+  let car = api.run("this.get_car", {vehicle: params.vehicle})[0];
+  const parameters = {};
+  parameters.calendarId = calendar_id;
+  parameters.$body = {
+    summary : `${params.requester} using ${car.Description}`,
+    start : {
+      dateTime : params.start
+    },
+    end : {
+      dateTime : params.end
+    },
+    attendees : [{'email': params.requester},{'email': 'lmc.carshare@gmail.com'}]
+  };
+  return api.run('google_calendar.create_calendar_event', parameters);
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://www.transposit.com/docs/references/js-operations
+ */
