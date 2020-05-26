33f9d8f023ac686d7f913d0daf8260d362ff64ea
Daisie Huang <daisie@transposit.com>
2020-05-26 09:04:50 +0000

    Commit from Transposit IDE

--- backend/request_received.js
+++ backend/request_received.js
@@ -4,24 +4,19 @@
   let data = JSON.parse(base64.decode(body.message.data));
   const parameters = {};
   parameters.userId = 'me';
-  parameters.startHistoryId = stash.get("historyId");
-  parameters.historyTypes = 'messageAdded';
-  let message = api.run('google_mail.list_history_of_mailbox', parameters);
-  
-  // {
-  //       "message": {
-  //         "data": "eyJlbWFpbEFkZHJlc3MiOiJsbWMuY2Fyc2hhcmVAZ21haWwuY29tIiwiaGlzdG9yeUlkIjoxODczfQ==",
-  //         "messageId": "945081858192555",
-  //         "message_id": "945081858192555",
-  //         "publishTime": "2020-01-21T01:21:18.422Z",
-  //         "publish_time": "2020-01-21T01:21:18.422Z"
-  //       },
-  //       "subscription": "projects/carshare-265707/subscriptions/request"
-  //     }
+  parameters.startHistoryId = parseInt(stash.get("historyId"));
+  //parameters.historyTypes = 'messageAdded';
+  let history = api.run('google_mail.list_history_of_mailbox', parameters)[0];
+    for (var i in history.messages) {
+      let messageId = history.messages[i].id;
+      let message = api.run("google_mail.get_message", { id: messageId, userId: "me"})[0];
+      console.log(message);
+      console.log(base64.decode(message.payload.body.data));
+    }
   return {
     status_code: 200,
     headers: { "Content-Type": "application/json" },
-    body: { message }
+    body: { history }
   };
 }
 
