54fd6da4c608a6ab15c6d6e85971ebef2d288354
Daisie Huang <daisie@transposit.com>
2020-05-26 08:02:56 +0000

    Commit from Transposit IDE

--- backend/send_car_conf_email.js
+++ backend/send_car_conf_email.js
@@ -1,9 +1,9 @@
 (params) => {
   const parameters = {};
-  parameters.to = 'params.requester';
+  parameters.to = params.requester;
   parameters.subject = 'A car request';
   parameters.message = `${params.uuid}`;
-  parameters[`from`] = 'lmc.carshare@gmail.com';
+  // parameters[`from`] = 'lmc.carshare@gmail.com';
   parameters.userId = 'me';
   return api.run('google_mail.send_message', parameters);
 }
