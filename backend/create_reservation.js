ac818d1c4ba8e8292904334a2ef6a29b5e484079
Daisie Huang <daisie@transposit.com>
2020-05-27 09:12:59 +0000

    Commit from Transposit IDE

--- backend/create_reservation.js
+++ backend/create_reservation.js
@@ -1,7 +1,42 @@
 (params) => {
   let calendar_id = api.run("this.get_calendarlist", {vehicle: params.vehicle})[0].id;
   let car = api.run("this.get_car", {vehicle: params.vehicle})[0];
-  const parameters = {};
+  
+  // send the request to the owner:
+  let request_params = { 
+    to: params.vehicle,
+    subject: 'Your vehicle has been requested',
+    message: `Your vehicle ${car.Description} has been requested for ${params.start} to ${params.end}.`,
+    userId: 'me'
+  };
+  let confirmed = true;
+  if (car.Confirm && params.confirmed == null) {
+    // we need to send an email to the car owner
+    confirmed = false;
+    request_params.message += `\nReply to this email to approve the request. Ignore this message if you don't want to approve it.`
+  } else {
+    // go ahead and make the reservation
+    request_params.to = params.requester;
+  }
+
+  let message = api.run('google_mail.send_message', request_params)[0];
+
+  // add the request to the google sheet
+  let parameters = {};
+  parameters.valueInputOption = 'USER_ENTERED';
+  parameters.range = '=Requests!A:F';
+  parameters.spreadsheetId = '1kyd3g0xuPYoyDuT6joT0gkl29YCFE56E2ktv6haRong';
+  parameters.responseValueRenderOption = 'FORMATTED_VALUE';
+  parameters.insertDataOption = 'INSERT_ROWS';
+  parameters.responseDateTimeRenderOption = 'FORMATTED_STRING';
+  parameters.$body = {
+//     threadId	vehicle	requester	start	end	confirmed
+    values : [ [ message.threadId, params.vehicle, params.requester, params.start, params.end, confirmed ] ]
+  };
+  return api.run('google_sheets.append_sheet_values', parameters);
+  
+  
+  parameters = {};
   parameters.calendarId = calendar_id;
   parameters.sendUpdates = 'all';
   parameters.$body = {
@@ -14,6 +49,8 @@
     },
     attendees : [{'email': params.requester, responseStatus: "accepted"},{'email': params.vehicle, responseStatus: "accepted"}]
   };
+  
+
   //return api.run('google_calendar.create_calendar_event', parameters);
   
 }
--- backend/send_car_conf_email.js
+++ backend/send_car_conf_email.js
@@ -3,8 +3,6 @@
   parameters.to = params.requester;
   parameters.subject = 'Your car has been requested';
   parameters.message = `${params.uuid}`;
-  parameters.contentType = "text/html";
-  parameters[`from`] = 'lmc.carshare@gmail.com';
   parameters.userId = 'me';
   return api.run('google_mail.send_message', parameters);
 }
