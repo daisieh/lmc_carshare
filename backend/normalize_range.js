8edbed4605f712d441757cd128249a5efacdd5bf
Daisie Huang <daisieh@gmail.com>
2020-10-28 04:01:59 +0000

    Commit from Transposit IDE

--- backend/normalize_range.js
+++ backend/normalize_range.js
@@ -1,9 +1,9 @@
 (params) => {
   const moment = require("moment-timezone-with-data.js");
-  let starttime = moment(params.start_datetime).format().replace("+00:00", "");
+  let starttime = moment(params.start_datetime).format();//.replace("+00:00", "");
   let start = moment.tz(starttime, "America/Vancouver");
   
-  let endtime = moment(params.end_datetime).format().replace("+00:00", "");
+  let endtime = moment(params.end_datetime).format();//.replace("+00:00", "");
   let end = moment.tz(endtime, "America/Vancouver");
   return {start, end};
 }
