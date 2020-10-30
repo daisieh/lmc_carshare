79db24952ec273766ce4c8a13dc6fecdc762cb4e
daisie_local <daisieh@gmail.com>
2020-10-30 12:47:30 -0700

    disable dates

--- src/index.tsx
+++ src/index.tsx
@@ -64,13 +64,13 @@ class SearchAvailabilityForm extends React.Component<SearchAvailabilityProps, Se
                     format="YYYY-MM-DD HH:mm"
                     onChange={this.handleStartChange}
                     value={new Date(this.props.startTimeValue)}
-                    disabled={!this.props.carsListed}
+                    disabled={this.props.carsListed}
                 />
                 <DatePicker
                     format="YYYY-MM-DD HH:mm"
                     onChange={this.handleEndChange}
                     value={new Date(this.props.endTimeValue)}
-                    disabled={!this.props.carsListed}
+                    disabled={this.props.carsListed}
                 />
                 <Button onClick={this.handleSubmit}>Submit</Button>
             </div>
