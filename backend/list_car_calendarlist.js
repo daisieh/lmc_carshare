7660f59a5b875db784fea25a056000bed5839d39
Daisie Huang <daisieh@gmail.com>
2020-11-05 23:29:58 +0000

    don't create calendars when listing

--- backend/list_car_calendarlist.js
+++ backend/list_car_calendarlist.js
@@ -1,25 +1,11 @@
 (params) => {
-  const _ = require("underscore.js");
   let cars = Object.keys(api.run("this.list_cars")[0]);
   let all_calendars = api.run('google_calendar.get_calendarlist', {}, {limit: 30});
   let calendars = {};
-  let calendar_names = [];
   for (var i in all_calendars) {
     if (cars.includes(all_calendars[i].summary)) {
       calendars[all_calendars[i].summary] = all_calendars[i];
-      calendar_names.push(all_calendars[i].summary);
     }
-  }
-  let cars_to_map = _.difference(cars, calendar_names);
-  for (var i in cars_to_map) {
-    console.log(`adding car: ${cars_to_map[i]}`);
-    const parameters = {};
-    parameters.$body = {
-      summary : cars_to_map[i],
-      timeZone : 'America/Vancouver'
-    };
-    let cal = api.run('google_calendar.create_calendar', parameters)[0];
-    calendars[cars_to_map[i]] = cal;
   }
     
   return calendars;
