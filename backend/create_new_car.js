f3c85dd1ee64109f892b22a8603fae41f4f77743
Daisie Huang <daisieh@gmail.com>
2020-11-05 23:26:45 +0000

    car methods

--- backend/create_new_car.js
+++ backend/create_new_car.js
@@ -2,21 +2,54 @@
   if (params.Licence == null || params.Licence == "") {
     throw "Licence plate is required"
   }
+  
+  let new_car =   {
+    "Timestamp": (new Date()).toString(),
+    "Make": params.Make,
+    "Model": params.Model,
+    "Color": params.Color,
+    "Features": params.Features,
+    "Licence": params.Licence,
+    "Email": params.Email,
+    "AlwaysAvailable": params.Availability,
+    "Confirm": params.Confirm
+  }
+  
   let cars = api.run("this.list_cars");
   let values = [];
   values.push(cars.shift());
   // look for the car to see if it needs to be updated
-  let is_updated = false;
+  let is_new_car = true;
   for (var i in cars) {
-    if (cars[i].Licence === params.Licence) {
-      console.log (`updating ${params.Licence}`);
-      cars[i] = params;
-      is_updated = true;
+    if (cars[i].Licence === new_car.Licence) {
+      console.log (`updating ${new_car.Licence}`);
+      cars[i] = new_car;
+      is_new_car = false;
     }
   }
-  if (is_updated == false) {
-    // append to the end:
-    cars.push(params);
+  
+  if (is_new_car == true) {
+    cars.push(new_car);
+    // create calendar for bookings:
+    let parameters = {};
+    parameters.$body = {
+      summary : new_car.Licence,
+      timeZone : 'America/Vancouver'
+    };
+    try {
+      api.run('google_calendar.create_calendar', parameters)[0];
+    } catch (e) {
+      console.log(e);
+    }
+    
+    if (!new_car.AlwaysAvailable) {
+      parameters.$body.summary = `${new_car.Licence}_available`;
+      try {
+        api.run('google_calendar.create_calendar', parameters)[0];
+      } catch (e) {
+        console.log(e);
+      }
+    }
   }
   
   // convert back to array for sheet:
@@ -25,10 +58,7 @@
   parameters.spreadsheetId = '1kyd3g0xuPYoyDuT6joT0gkl29YCFE56E2ktv6haRong';
   parameters.valueInputOption = "RAW";
   for (var i in cars) {
-    let val = [];
-    for (var j in values[0]) {
-      val.push(cars[i][values[0][j]]);
-    }
+    let val = api.run("this.car_object_to_sheet_row", {car_object: cars[i]});
     values.push(val);
   }
   parameters.$body = { values : values };
