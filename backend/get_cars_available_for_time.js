75ea5c126022c1a736cdc1922feb8addab3ecdea
Daisie Huang <daisieh@gmail.com>
2020-11-05 23:40:57 +0000

    Commit from Transposit IDE

--- backend/get_cars_available_for_time.js
+++ backend/get_cars_available_for_time.js
@@ -8,7 +8,7 @@
   let available_cars = [];
   let calendar_ids = [];
   for (var i in calendars) {
-    calendar_ids.push({id: calendars[i].id});
+    calendar_ids.push({id: calendars[i]});
   }
   let freebusy = api.run('google_calendar.get_calendars_freebusy', {$body: { timeMax : endTime, timeMin : startTime, items : calendar_ids , timeZone: 'America/Vancouver'}})[0];
   
@@ -16,13 +16,13 @@
     let car = i.replace("_available", "");
     if (!cars[car].AlwaysAvailable) {
       console.log(`checking to see if (default busy) ${cars[car].Description} is busy on cal ${i}...`);
-      if (freebusy.calendars[calendars[i].id].busy.length > 0) {
+      if (freebusy.calendars[calendars[i]].busy.length > 0) {
         // console.log(`...listed as busy, so it's available`);
         available_cars.push(cars[car]);
       }
     } else {
       console.log(`checking to see if (default available) ${cars[car].Description} is busy...`);
-      if (freebusy.calendars[calendars[i].id].busy.length == 0) {
+      if (freebusy.calendars[calendars[i]].busy.length == 0) {
         // console.log("...not busy, so it's available");
         available_cars.push(cars[car]);
       }
