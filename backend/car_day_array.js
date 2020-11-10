bcc47e53e8edda2caf797d1be73de5568acd8d6c
Daisie Huang <daisieh@gmail.com>
2020-11-10 03:33:42 +0000

    clean up busy_schedule

--- backend/car_day_array.js
+++ backend/car_day_array.js
@@ -7,26 +7,30 @@
   let car_events = api.run("this.list_car_busy_schedule", {start: start, end: end})[0];
   car_events.interval = interval;
   // return car_events;
-  for (var x = 0; x < 3; x++) {
+  // for (var x = 0; x < 3; x++) {
     for (var i in car_events.cars) {
       let car = car_events.cars[i];
       let events = car_events.car_events[i];
       let hours = ',';
-      let current = moment(start).format('X');
+      let current = parseInt(moment(start).format('X'));
       while (events.length > 0) {
         let event = events.shift();
         console.log(event);
-        let this_start = moment(event.start).format('X');
-        let this_end = moment(event.end).format('X');
-        let freespan = (parseInt(this_start) - parseInt(current))/interval;
-        let busyspan = (parseInt(this_end) - parseInt(this_start))/interval;
+        let this_start = parseInt(moment(event.start).format('X'));
+        let this_end = parseInt(moment(event.end).format('X'));
+        let freespan = this_start - current;
+        let busyspan = this_end - this_start;
         console.log(`${freespan} ${busyspan}`)
-        hours += `${",".repeat(freespan)}${"1".repeat(busyspan)}`;
+//        hours += `${",".repeat(freespan)}${"1".repeat(busyspan)}`;
+        hours += `${freespan} ${busyspan} `;
+
         current = this_end;
         console.log(hours);
       }
-      let freespan = (parseInt(moment(end).format('X')) - parseInt(current))/interval;
-      hours += ",".repeat(freespan) + ",";
+      let this_end = parseInt(moment(end).format('X'));
+      let freespan = this_end - current;
+      hours += `${freespan},`;
+      // hours += ",".repeat(freespan) + ",";
 //       // replace 1-char blocks:
 //       hours = hours.replace(/,1,/g,",o,");
 
@@ -46,7 +50,7 @@
     start = start.add(1,"day");
     end = end.add(1,"day");
     car_events = api.run("this.list_car_busy_schedule", {start: start, end: end})[0];
-  }
+  // }
   car_events.busy_segments = car_lines;
   car_events.interval = interval;
 
