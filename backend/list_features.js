335a7031228175f2194af2b814285378306b0dcb
Daisie Huang <daisieh@gmail.com>
2020-11-10 07:06:30 +0000

    all_features

--- backend/list_features.js
+++ backend/list_features.js
@@ -1,6 +1,16 @@
 (params) => {
-  let cars = api.run("this.list_cars")[0];
   let features = [];
+
+  if (params.all_features) {
+      const parameters = { range: 'Tags!A1:A30', spreadsheetId: '1kyd3g0xuPYoyDuT6joT0gkl29YCFE56E2ktv6haRong' };
+      let sheet_rows = api.run('google_sheets.get_sheet_values', parameters)[0].values;
+      for (var i in sheet_rows) {
+        features.push(...sheet_rows[i]);
+      }
+
+      return features;
+  }
+  let cars = api.run("this.list_cars")[0];
   for (var i in cars) {
     let this_feat = cars[i].Features;
     for (var j in this_feat) {
