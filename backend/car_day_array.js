ffc11e9ad72189cc7124be2fc3890c6b8107798d
Daisie Huang <daisieh@gmail.com>
2020-11-10 03:53:48 +0000

    Commit from Transposit IDE

--- backend/car_day_array.js
+++ backend/car_day_array.js
@@ -3,11 +3,9 @@
   const interval = parseInt(params.interval); // number of secs in one hour is 3600
   let start = moment.tz(params.start,"America/Vancouver").format("YYYY-MM-DDTHH:mm:00Z");
   let end = moment.tz(params.start,"America/Vancouver").add(3,"day").format("YYYY-MM-DDTHH:mm:00Z");
-  console.log(start, end)
   let car_lines = [];
   let car_events = api.run("this.list_car_busy_schedule", {start: start, end: end})[0];
   car_events.interval = interval;
-  // return car_events;
     for (var i in car_events.cars) {
       let car = car_events.cars[i];
       let events = car_events.car_events[i];
@@ -15,36 +13,33 @@
       let current = parseInt(moment(start).format('X')/interval);
       while (events.length > 0) {
         let event = events.shift();
-        console.log(event);
         let this_start = parseInt(moment(event.start).format('X')/interval);
         let this_end = parseInt(moment(event.end).format('X')/interval);
         let freespan = this_start - current;
         let busyspan = this_end - this_start;
-        console.log(`${freespan} ${busyspan}`)
-//        hours += `${",".repeat(freespan)}${"1".repeat(busyspan)}`;
-        hours += `${freespan} ${busyspan} `;
+       hours += `${",".repeat(freespan)}${"1".repeat(busyspan)}`;
+        // hours += `${freespan} ${busyspan} `;
 
         current = this_end;
-        console.log(hours);
       }
       let this_end = parseInt(moment(end).format('X')/interval);
       let freespan = this_end - current;
-      hours += `${freespan},`;
-      // hours += ",".repeat(freespan) + ",";
-//       // replace 1-char blocks:
-//       hours = hours.replace(/,1,/g,",o,");
+      // hours += `${freespan},`;
+      hours += ",".repeat(freespan) + ",";
+      // replace 1-char blocks:
+      hours = hours.replace(/,1,/g,",o,");
 
-//       // replace 2-char blocks:
-//       hours = hours.replace(/,11,/g,",<>,");
+      // replace 2-char blocks:
+      hours = hours.replace(/,11,/g,",<>,");
 
-//       // replace 3-char blocks:
-//       hours = hours.replace(/,111,/g,",<->,");
+      // replace 3-char blocks:
+      hours = hours.replace(/,111,/g,",<->,");
 
-//       // replace more than 4-char blocks:
-//       hours = hours.replace(/,11/g,",<-").replace(/11,/g,"->,").replace(/1/g,"-");
+      // replace more than 4-char blocks:
+      hours = hours.replace(/,11/g,",<-").replace(/11,/g,"->,").replace(/1/g,"-");
 
-//       // trim off ends
-//       hours = hours.replace(/^,/,"").replace(/,$/,"");
+      // trim off ends
+      hours = hours.replace(/^,/,"").replace(/,$/,"");
       car_lines.push(hours);
     }
   car_events.busy_segments = car_lines;
