af2f997a009427d8cfa3465cb617545049abaed6
Daisie Huang <daisieh@gmail.com>
2020-12-03 02:00:54 +0000

    Commit from Transposit IDE

--- backend/list_features.js
+++ backend/list_features.js
@@ -10,7 +10,7 @@
 
       return features;
   }
-  let cars = api.run("this.list_cars");
+  let cars = api.run("this.list_cars")[0];
   for (var i in cars) {
     let this_feat = cars[i].Features;
     for (var j in this_feat) {
