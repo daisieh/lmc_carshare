5d8d019f127dd33cf722c1ee098e3aaae27827cf
Daisie Huang <daisieh@gmail.com>
2020-12-05 09:12:24 +0000

    delete_car

--- backend/untitled.js
+++ backend/untitled.js
@@ -1,9 +1,17 @@
 (params) => {
   const moment = require('moment-timezone-with-data.js');
 //  return moment.tz('2020-11-24 00:00', "America/Vancouver")
-    const parameters = {};
-  parameters.calendarId = '5fc1dl3ok9ae6efnd7avuf7om8@group.calendar.google.com';
-  return api.run('google_calendar.get_acl_rules', parameters, {limit: 10});
+
+  const parameters = { range: 'Cars!A1:Z50', spreadsheetId: env.get("spreadsheet_id") };
+  let sheet_rows = api.run('google_sheets.get_sheet_values', parameters)[0].values;
+  let rows_to_keep = [sheet_rows.shift()];
+  let licence_index = rows_to_keep[0].indexOf("Licence plate");
+  for (var i in sheet_rows) {
+    if (sheet_rows[i][licence_index] !== "AL675T") {
+      rows_to_keep.push(sheet_rows[i]);
+    }
+  }
+  return rows_to_keep;
 }
 
 /*
