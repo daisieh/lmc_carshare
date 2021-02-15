ec68e9b9ee0df22555bb3e7259ae7e0147c17a52
Daisie Huang <daisieh@gmail.com>
2021-02-15 05:46:51 +0000

    Commit from Transposit IDE

--- backend/delete_car.js
+++ backend/delete_car.js
@@ -1,27 +1,7 @@
 (params) => {
-  // remove all associated requests
-  let requests = api.run("this.list_requests");
-  let to_delete = [];
-  for (var i in requests) {
-    if (requests[i].vehicle === params.licence) {
-      to_delete.push(requests[i].eventId);
-    }
-  }
-  let results = api.run("this.delete_requests", {eventIds: to_delete});
-  console.log(results);
-  // remove the calendars
-  let all_calendars = api.run('google_calendar.get_calendarlist');
-  let delete_cals = [];
-  for (var i in all_calendars) {
-    if (all_calendars[i].summary.includes("params.licence") || all_calendars[i].summary.includes(`${params.licence}_available`)) {
-      delete_cals.push(all_calendars[i].id);
-    }
-  }
-  console.log(delete_cals);
-  for (var i in delete_cals) {
-    api.run('google_calendar.delete_calendar', {calendarId: delete_cals[i]});
-  }
-  // remove the car
+  let this_car;
+  
+  // find the car
   let parameters = { range: 'Cars!A1:Z50', spreadsheetId: env.get("spreadsheet_id") };
   let sheet_rows = api.run('google_sheets.get_sheet_values', parameters)[0].values;
   let rows_to_keep = [sheet_rows.shift()];
@@ -30,9 +10,50 @@
     let row = sheet_rows[i];
     if (sheet_rows[i][licence_index] !== params.licence) {
       rows_to_keep.push(sheet_rows[i]);
+    } else {
+      this_car = api.run('this.car_sheet_row_to_object', {sheet_row: sheet_rows[i]})[0];
     }
   }
+  
+  if (this_car == null) {
+    throw `No car with licence ${params.licence}`;
+  }
 
+  // remove all associated requests
+  let requests = api.run("this.list_requests");
+  let to_delete = [];
+  for (var i in requests) {
+    if (requests[i].vehicle === this_car.Licence) {
+      to_delete.push(requests[i].eventId);
+    }
+  }
+  let results = api.run("this.delete_requests", {eventIds: to_delete});
+  console.log(`deleted ${results}`);
+  
+  // remove the calendars
+  try {
+    api.run('google_calendar.delete_calendar', {calendarId: this_car.BookingCalendar});
+  } catch (e) {
+    console.log(e.message);
+  }
+  try {
+    api.run('google_calendar.delete_calendar', {calendarId: this_car.AvailableCalendar});
+  } catch (e) {
+    console.log(e.message);
+  }
+  
+  // let all_calendars = api.run('google_calendar.get_calendarlist');
+  // let delete_cals = [];
+  // for (var i in all_calendars) {
+  //   if (all_calendars[i].summary.includes("params.licence") || all_calendars[i].summary.includes(`${params.licence}_available`)) {
+  //     delete_cals.push(all_calendars[i].id);
+  //   }
+  // }
+  // console.log(delete_cals);
+  // for (var i in delete_cals) {
+  //   api.run('google_calendar.delete_calendar', {calendarId: delete_cals[i]});
+  // }
+  
   // clear sheet range:
   parameters.$body = { };
   api.run('google_sheets.clear_sheet_values', parameters);
@@ -40,5 +61,6 @@
   parameters.valueInputOption = "RAW";
   parameters.$body = { values : rows_to_keep };
   let result = api.run('google_sheets.update_sheet_values', parameters);
+
   return result;
 }
